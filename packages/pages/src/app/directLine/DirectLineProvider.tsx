34b04c850b1001b28afe6286d345b4efc646f510
William Wong <compulim@users.noreply.github.com>
2024-04-15 18:00:59 +0000

    Remove React

--- packages/pages/src/app/directLine/DirectLineProvider.tsx
+++ packages/pages/src/app/directLine/DirectLineProvider.tsx
@@ -1,4 +1,4 @@
-import React, { memo, type ReactNode } from 'react';
+import { memo, type ReactNode } from 'react';
 
 import DirectLineEmulatorProvider from './DirectLineEmulatorProvider';
 import DirectLineMockBotProvider from './DirectLineMockBotProvider';
